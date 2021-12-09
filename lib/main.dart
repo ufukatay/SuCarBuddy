5037130d3520b8b6d5a8bac1c98e8c6e158781e5
sergio <mustafasergen@sabanciuniv.edu>
2021-12-09 21:36:47 +0300

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,75 @@ import 'package:projedeneme/routes/signup.dart';
 import 'package:projedeneme/routes/login.dart';
 import 'package:projedeneme/routes/WalkThrough.dart';
 import 'package:projedeneme/routes/condition.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
 
 
 void main() {
-  runApp( MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //home: LoginSign(),
-      initialRoute: '/Condition' ,
-      routes: {
-        '/Condition': (context) => Condition(),
-        '/': (context) => Welcome(),
-        '/WalkThrough': (context) => WalkThrough(),
-        '/login': (context) => Login(),
-        '/signup': (context) => SignUp(),
-      }
-  )
-  );
+
+  runApp(MyFirebaseApp());
 }
+
+class MyFirebaseApp extends StatefulWidget {
+  const MyFirebaseApp({Key? key}) : super(key: key);
+
+  @override
+  _MyFirebaseAppState createState() => _MyFirebaseAppState();
+}
+
+class _MyFirebaseAppState extends State<MyFirebaseApp> {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot){
+        if(snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('No Firebase Connection: ${snapshot.error.toString()}'),
+              ),
+            ),
+          );
+        }
+        if(snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            home: MyApp(),
+          );
+        }
+        return MaterialApp(
+          home: Center(
+            child: Text('Connecting to Firebase'),
+          ),
+        );
+      },);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //home: LoginSign(),
+        initialRoute: '/Condition' ,
+        routes: {
+          '/Condition': (context) => Condition(),
+          '/': (context) => Welcome(),
+          '/WalkThrough': (context) => WalkThrough(),
+          '/login': (context) => Login(),
+          '/signup': (context) => SignUp(),
+        }
+    );
+  }
+}
+
 
 
 
